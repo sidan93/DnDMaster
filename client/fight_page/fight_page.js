51e9f2e7fb0a8d5eeecb410fe67bbb0b34a984b4
Sidorov Andrey <sidan93@gmail.com>
2016-09-13 18:52:02 +0700

    Сделал чтобы вкладка боя выглядела более компактно

--- client/fight_page/fight_page.js
+++ client/fight_page/fight_page.js
@@ -19,20 +19,20 @@ Template.fight_page.helpers({
 });
 
 Template.fight_page.events({
-	'click .modifier': function(event) {
-		if (event.target.className != 'rem' && event.target.className != 'add')
+	'click .click_value': function(event) {
+		if (!($(event.target).hasClass('rem') || $(event.target).hasClass('add')))
 			return;
 
 		var modifier = $(event.target).parents('.modifier_title')[0].getAttribute('name');
 		var form = $(event.target).parents('form')[0];
 		// Модификатор который отвечает, за то складываем мы статы или вычитаем
 		modifierDamage = 1;
-		if (event.target.className == 'rem')
+		if ($(event.target).hasClass('rem'))
 			modifierDamage = -1;
 		var character = CharacterList.findOne({_id: form.name}).character;
 		
 		if (modifier == 'hp') {
-			var value = parseInt(form.hp_change.value || 0) || 0;
+			var value = parseInt(form.hp_change.value || 0) || 1;
 			if (character.fight == null)
 				character.fight = {};
 			character.fight.hp_current = Math.min((character.fight.hp_current || character.props.hp) + value * modifierDamage, character.props.hp);
@@ -46,11 +46,11 @@ Template.fight_page.events({
 			form.hp_change.value = null;
 		}
 		if (modifier == 'max_hp') {
-			var value = parseInt(form.hp_change.value || 0) || 0;
+			var value = parseInt(form.hp_change.value || 0) || 1;
 			character.props.hp += value * modifierDamage;
 		}
 		if (modifier == 'damage') {
-			var value = parseInt(form.set_damage.value || 0) || 0;
+			var value = parseInt(form.damage_value.value || 0) || 1;
 			var modifier_value = Math.max(((character.main_props == 'str' ? character.props.str : character.props.agi) - 10) / 2 + 1, 1)
 			character.exp.melle += value * modifier_value;
 		}
@@ -64,29 +64,35 @@ Template.fight_page.events({
 				'dark': 'shine'
 			};
 			var school = modifier.replace('damage_', '');
-			var value = parseInt(form['set_damage_' + school].value || 0) || 0;
+			var value = parseInt(form.damage_value.value || 0) || 1;
+			var stats = value * ((character.props.wis - 10) / 2 + 1);
 			var stats = value * ((character.props.wis - 10) / 2 + 1);
 			character.exp.magic[school] += stats;
 			character.exp.magic[anti[school]] = Math.max(character.exp.magic[anti[school]] - stats / 2, 0)
 		}
-		if (modifier == 'exp_melle') {
-			var value = parseInt(form.exp_melle_change.value || 0) || 0;
-			character.exp.melle += value * modifierDamage;
-		}
  		if (modifier == 'exp_hp') {
-			var value = parseInt(form.exp_hp_change.value || 0) || 0;
-			character.exp.hp += value * modifierDamage;
+			var value = parseInt(form.exp_value.value || 0) || 1;
+			character.exp.hp = Math.max(character.exp.hp + value * modifierDamage, 0)
 		}
-		if (['str', 'agi', 'sta', 'int', 'wis', 'cha', 'mas'].indexOf(modifier) != -1) {
-			character.props[modifier] += modifierDamage;
+		if (modifier == 'exp_melle') {
+			var value = parseInt(form.exp_value.value || 0) || 1;
+			character.exp.melle = Math.max(character.exp.melle + value * modifierDamage, 0)
 		}
 		if (['exp_fire', 'exp_water','exp_earth', 'exp_air', 'exp_shine', 'exp_dark'].indexOf(modifier) != -1) {
-			var value = parseInt(form[modifier + '_change'].value || 0) || 0;
+			var value = parseInt(form.exp_value.value || 0) || 1;
 			var school = modifier.replace('exp_', '');
-			character.exp.magic[school] += value * modifierDamage;
+			character.exp.magic[school] = Math.max(character.exp.magic[school] + value * modifierDamage, 0);
 
 		}
 		$('.fight_page form').trigger('reset'); 
 		Meteor.call('UpdateCharacter', form.name, character);
 	}
 });
+
+
+// Получить модификатор характеристики
+// stats - значение харатеристики
+// default_value - значение которое необходим добавить, по умолчанию = 1
+function get_modifier(stats, default_value) {
+	return parseInt((stats - 10) / 2) + (default_value || 1);
+}
