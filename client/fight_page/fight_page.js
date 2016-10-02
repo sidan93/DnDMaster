e9a058088afd57d098c1047e243b826c385847a5
Sidorov Andrey <sidan93@gmail.com>
2016-10-02 10:19:54 +0700

    Добавил уровни в страницу боя

--- client/fight_page/fight_page.js
+++ client/fight_page/fight_page.js
@@ -19,118 +19,47 @@ Template.fight_page.helpers({
 
 	get_dmg_list: function(main) {
 		return [
-			{
-				title: 'Ближним',
-				name: 'damage',
-				visible: true
-			},
-			{
-				title: 'Огнем',
-				name: 'damage_fire',
-				visible: main.fire
-			},
-			{
-				title: 'Водой',
-				name: 'damage_water',
-				visible: main.water
-			},
-			{
-				title: 'Землей',
-				name: 'damage_earth',
-				visible: main.earth
-			},
-			{
-				title: 'Воздухом',
-				name: 'damage_air',
-				visible: main.air
-			},
-			{
-				title: 'Светом',
-				name: 'damage_shine',
-				visible: main.shine
-			},
-			{
-				title: 'Тьмой',
-				name: 'damage_dark',
-				visible: main.dark
-			},
+			{ title: 'Ближним', name: 'damage', visible: true },
+			{ title: 'Огнем', name: 'damage_fire', visible: main.fire },
+			{ title: 'Водой', name: 'damage_water', visible: main.water },
+			{ title: 'Землей', name: 'damage_earth', visible: main.earth },
+			{ title: 'Воздухом', name: 'damage_air', visible: main.air },
+			{ title: 'Светом', name: 'damage_shine', visible: main.shine },
+			{ title: 'Тьмой', name: 'damage_dark', visible: main.dark }
 		];
 	},
 
 	get_exp_list: function(exp, main) {
 		return [
-			{
-				title: 'Персонаж:',
-				value: exp.hero,
-				name: 'exp_hero',
-				visible: true
-			},
-			{
-				title: 'Здоровье:',
-				value: exp.hp,
-				name: 'exp_hp',
-				visible: true				
-			},
-			{
-				title: 'Ближнего боя:',
-				value: exp.melle,
-				name: 'exp_melle',
-				visible: true				
-			},
-			{
-				title: 'Магии огня:',
-				value: exp.magic.fire,
-				name: 'exp_fire',
-				visible: main.fire				
-			},
-			{
-				title: 'Магии воды:',
-				value: exp.magic.water,
-				name: 'exp_water',
-				visible: main.water				
-			},
-			{
-				title: 'Магии земли:',
-				value: exp.magic.earth,
-				name: 'exp_earth',
-				visible: main.earth				
-			},
-			{
-				title: 'Магии воздуха:',
-				value: exp.magic.air,
-				name: 'exp_air',
-				visible: main.air				
-			},
-			{
-				title: 'Магии света:',
-				value: exp.magic.shine,
-				name: 'exp_shine',
-				visible: main.shine				
-			},
-			{
-				title: 'Магии тьмы:',
-				value: exp.magic.dark,
-				name: 'exp_dark',
-				visible: main.dark				
-			},
-			{
-				title: 'Доп 1:',
-				value: exp.other.attr1,
-				name: 'exp_attr1',
-				visible: false				
-			},
-			{
-				title: 'Доп 2:',
-				value: exp.other.attr2,
-				name: 'exp_attr2',
-				visible: false				
-			},
-			{
-				title: 'Доп 3:',
-				value: exp.other.attr3,
-				name: 'exp_attr3',
-				visible: false				
-			},
+			{ title: 'Персонаж:', value: exp.hero, name: 'exp_hero', visible: true },
+			{ title: 'Здоровье:', value: exp.hp, name: 'exp_hp', visible: true },
+			{ title: 'Ближнего боя:', value: exp.melle, name: 'exp_melle', visible: true },
+			{ title: '', value: null, name: null, visible: true }, // Пустая строка между физ уроном и магией
+			{ title: 'Магии огня:', value: exp.magic.fire, name: 'exp_fire', visible: main.fire },
+			{ title: 'Магии воды:', value: exp.magic.water, name: 'exp_water', visible: main.water },
+			{ title: 'Магии земли:', value: exp.magic.earth, name: 'exp_earth', visible: main.earth },
+			{ title: 'Магии воздуха:', value: exp.magic.air, name: 'exp_air', visible: main.air },
+			{ title: 'Магии света:', value: exp.magic.shine, name: 'exp_shine', visible: main.shine },
+			{ title: 'Магии тьмы:', value: exp.magic.dark, name: 'exp_dark', visible: main.dark },
+			{ title: 'Доп 1:', value: exp.other.attr1, name: 'exp_attr1', visible: false },
+			{ title: 'Доп 2:', value: exp.other.attr2, name: 'exp_attr2', visible: false },
+			{ title: 'Доп 3:', value: exp.other.attr3, name: 'exp_attr3', visible: false }
+		];
+	},
+
+	get_lvl_list: function(lvl, main) {
+		return [
+			{ title: 'Героя:', value: lvl.hero, name:'lvl_hero', visible: true },
+			{ title: 'Жизней:', value: lvl.hp, name:'lvl_hp', visible: true },
+			{ title: 'Попадания:', value: lvl.hit, name:'lvl_hit', visible: true },
+			{ title: 'Урона:', value: lvl.dmg, name:'lvl_dmg', visible: true },
+
+			{ title: 'Огня:', value: lvl.fire, name:'lvl_fire', visible: main.fire },
+			{ title: 'Воды:', value: lvl.water, name:'lvl_water', visible: main.water },
+			{ title: 'Земли:', value: lvl.earth, name:'lvl_earth', visible: main.earth },
+			{ title: 'Воздуха:', value: lvl.air, name:'lvl_air', visible: main.air },
+			{ title: 'Света:', value: lvl.shine, name:'lvl_shine', visible: main.shine },
+			{ title: 'Тьмы:', value: lvl.dark, name:'lvl_dark', visible: main.dark }
 		];
 	}
 });
@@ -206,6 +135,11 @@ Template.fight_page.events({
 			var value = parseInt(form.exp_value.value || 0) || 1;
 			var attr = modifier.replace('exp_', '');
 			character.exp.other[attr] = Math.max(character.exp.other[attr] + value * modifierDamage, 0);
+		}
+		if (['lvl_hp', 'lvl_hit','lvl_hero', 'lvl_dmg', 'lvl_fire', 'lvl_water',
+			'lvl_earth', 'lvl_air', 'lvl_shine', 'lvl_dark'].indexOf(modifier) != -1) {
+			var value = modifier.replace('lvl_', '');
+			character.lvls[value] = Math.min(Math.max(character.lvls[value] + modifierDamage, 0), 20);
 		}
 		if (['str', 'agi', 'sta', 'int', 'wis', 'cha', 'mas'].indexOf(modifier) != -1) {
 			character.props[modifier] = Math.min(Math.max(character.props[modifier] + modifierDamage, 0), 20);
