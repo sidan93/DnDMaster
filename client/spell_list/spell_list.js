fcdfe357f10a7da71f1d5b09805b031eb3de0e4f
ab.sidorov <ab.sidorov@tensor.ru>
2016-10-21 19:45:30 +0700

    Сделал классную распределение заклинаний с группировкой по названию+Тип+школа

--- client/spell_list/spell_list.js
+++ client/spell_list/spell_list.js
@@ -5,16 +5,15 @@ import { Mongo } from 'meteor/mongo';
 SpellList = new Mongo.Collection('spell_list');
 
 Template.spell_list_page.events({
+	'click .spell_nav .nav_spell': function() {
+		Session.set('selected_nav_spell', getUniqValue(this));
+	},
 	'click .spell_list .spell': function() {
 		Session.set('selected_spell', this._id);
 	},
 
 	'click .spell_upsert': function() {
 		spell_upsert(document.forms.spell_edit);
-	},
-	'keypress .spell_edit': function(event) {
-		if (event.keyCode == 13)
-			spell_upsert(document.forms.spell_edit);
 	},
 	'click .spell_delete': function() {
 		Meteor.call('DeleteSpell', Session.get('selected_spell'));
@@ -26,8 +25,62 @@ Template.spell_list_page.events({
 });
 
 Template.spell_list_page.helpers({
-	get_spell_list: function() {
-		return SpellList.find({}, {sort: { lvl:-1, type:1, name: 1 }});
+	get_nav_spell_list: function () {
+		return _.unique(SpellList.find({}, {sort: {name: 1}}).fetch(), false, function(i) {
+			return getUniqValue(i);
+		});
+	},
+
+	get_ritual_spell_list: function(needCount) {
+		var nav_spell = Session.get('selected_nav_spell');
+		var result = SpellList.find(
+			{ $where: function() 
+				{
+					return this.ritual != null && !!this.users && isUniq(this, nav_spell);
+				}
+			}, 
+			{ sort: { lvl:-1, type:1, name: 1 } }
+		);
+
+		if (needCount)
+			return result.count();
+		return result;
+	},
+
+	get_non_ritual_spell_list: function(needCount) {
+		var nav_spell = Session.get('selected_nav_spell');
+		var result = SpellList.find(
+			{ $where: function() 
+				{
+					return this.ritual == null && !!this.users && isUniq(this, nav_spell);
+				}
+			}, 
+			{ sort: { lvl:-1, type:1, name: 1 } }
+		);
+
+		if (needCount)
+			return result.count();
+		return result;
+	},
+	get_non_spell_list: function(needCount) {
+		var nav_spell = Session.get('selected_nav_spell');
+		var result = SpellList.find(
+			{ $where: function() 
+				{
+					return !this.users && isUniq(this, nav_spell);
+				} 
+			}, 
+			{ sort: { lvl:-1, type:1, name: 1 } }
+		);
+
+		if (needCount)
+			return result.count();
+		return result;
+	},
+
+	selected_nav_spell: function() {
+		if (isUniq(this, Session.get('selected_nav_spell')))
+			return 'selected';
 	},
 	selected_spell: function() {
 		if (this._id == Session.get('selected_spell'))
@@ -47,6 +100,7 @@ function spell_upsert(form) {
 		lvl: parseInt(form.lvl.value) || 0,
 		points: parseInt(form.points.value) || 0,
 		type: form.type.value,
+		ritual: form.ritual.value || null,
 		school: form.school.value,
 		dices: form.dices.value,
 		effect: form.effect.value,
@@ -73,4 +127,16 @@ function spell_upsert(form) {
 		Meteor.call('UpdateSpell', id, spell, after_save);
 	else 
 		Meteor.call('AddSpell', spell, after_save);
+}
+
+
+function getUniqValue(obj) {
+	return obj.name + '||' + obj.type + '||' + obj.school;
+}
+function isUniq(obj, value) {
+	if (!value)
+		return false;
+
+	var values = value.split('||');
+	return obj.name == values[0] && obj.type == values[1] && obj.school == values[2];
 }
